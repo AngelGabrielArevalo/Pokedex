e059cdb89ee434cb0018cdc489915ef835b863df
agareval <angel.arevalo@connectis-ict.com.ar>
2022-10-06 16:47:09 -0300

    Creo la seed para cargar la db

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@nestjs/common';
-import { AxiosInstance } from 'axios';
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-responce.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
 
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
   async executeSeed() {
+    await this.pokemonModel.deleteMany({}).exec();
     const URL = 'https://pokeapi.co/api/v2/pokemon?limit=650';
     const { data } = await this.axios.get<PokeResponse>(URL);
     const pokemones = data.results.map((poke) => {
@@ -17,6 +25,7 @@ export class SeedService {
         nroPoke: +segmentos[segmentos.length - 2],
       };
     });
+    await this.pokemonModel.insertMany(pokemones);
     return pokemones;
   }
 }
